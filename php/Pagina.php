0df33415edb75bf94ffa775f69e46362ac7e831d
RamirezDei <90815556+RamirezDei@users.noreply.github.com>
2021-10-06 23:01:53 -0500

    Add files via upload

---
+++ php/Pagina.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/diseño.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
+    <title>Guild</title>
+</head>
+<body bgcolor="D3D0CB">
+    <header>
+        <div>
+            <h2 class="main_title">GUILD</h2>
+        </div>
+
+        <nav>
+           <input class="barra" type="text" name="Buscar" id="Buscar" placeholder="Realizar tu Busqueda">
+            <ul class="page_options">
+                <li class="nav_option">Categorias</li>
+                <li class="nav_option">Favoritos</li>
+                <li class="nav_option">Mis compras</li>
+                <li class="nav_option">Ofertas</li>
+                <li class="nav_option">Vender</li>
+            </ul>  
+        </nav>
+        <ul>
+            <li class="nav_option">Iniciar sesión</li>
+            <li class="nav_option">Colombia</li>
+            </ul> 
+    </header>
+    <main>
+    <figure class="my_photo_image">
+        <img src="https://static2.abc.es/media/tecnologia/2020/11/21/FotoPandavideojuegos-U81126801005Xtw-1200x630@abc.jpg" width="1348" height="400"  alt="imagen del main">
+         </figure>
+        <div class="my_title">
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, magnam.</h1>
+        <h2> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione expedita sapiente blanditiis iste magni odit adipisci ipsam dolores, ipsa, corrupti neque, nostrum modi. Asperiores blanditiis corrupti necessitatibus ut facere alias?</h2>
+        <button type="button" class="btn">Ver más</button>
+        </div>
+    
+    </main>
+    <h1 class="my_name">Plataformas</h1>
+    <nav>
+       <header class="prueba">
+            <ul class="options">
+                <li class="option">PS4</li>
+                <li class="option">PC</li>
+                <li class="option">NINTENDO</li>
+            </ul>
+        </nav>
+</header>
+
+
+    <div class="container">
+    <section class="projects">
+        <h1 class="my_name">Últimas noticias</h1>
+        <div class="union_sections">
+          <div>
+            <figure class="mockup_image">
+                <img src="https://img.freepik.com/psd-gratis/banner-jugador-efecto-falla_1419-2359.jpg?size=626&ext=jpg&ga=GA1.2.1728032082.1631577600" width="150" height="150" alt="imagen_del_proyecto">
+                <img src="https://img.freepik.com/psd-gratis/elementos-divertidos-videojuegos_1419-2367.jpg?size=338&ext=jpg" width="150" height="150" alt="imagen_batalla">
+                <img src="https://image.freepik.com/vector-gratis/jugadores-juegan-videojuegos-diferentes-plataformas-hardware-concepto-juego-multiplataforma-multiplataforma-juego-cruzado_335657-1819.jpg" width="150" height="150" alt="imagen_batalla">
+            </figure>
+        </div>
+        <div>
+         <h3>Lorem ipsum dolor sit. </h3>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit illum quia nemo beatae. Deserunt, voluptatem! Eius est temporibus ducimus accusantium veritatis sed repellendus aspernatur quas! Delectus quo tempore repellendus incidunt voluptatem ullam debitis, corrupti architecto. Sequi voluptates, blanditiis placeat, repudiandae eaque ipsam vitae soluta in dolorem ut officia vel itaque!</p>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione sint repellat eaque accusamus ab sequi praesentium dolore ipsum est iure unde consequuntur atque, quae debitis, eveniet mollitia natus ipsam nihil assumenda? Laudantium quam labore non neque modi aliquid ullam nam mollitia velit hic architecto aliquam quod, eveniet blanditiis obcaecati expedita.</h3> 
+         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta quisquam perspiciatis nobis odio distinctio laudantium at. Minima ab sed facere vitae? Odio quos fugiat sit magni nulla? Iure non quisquam distinctio obcaecati, aut soluta dolorum corporis? Harum porro beatae consequuntur dolore reiciendis vero tempora voluptatum neque, excepturi commodi, laboriosam quod!</p> 
+         <h3>Lorem ipsum dolor sit amet.</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore delectus eveniet beatae ullam. Inventore cumque totam alias! Dicta sunt expedita consequatur. Maiores sapiente laborum, placeat minima assumenda sunt ipsa distinctio veniam, vitae iusto provident eveniet soluta sit blanditiis illum atque voluptas sed et officiis ut aspernatur odio aut facilis quia. </p>    
+        </div>
+    </section>
+    </div>
+    </div>
+    
+    <footer> 
+        <span>
+            <a class="contacto" href="tienda">Tienda</a>
+            <ul>
+              <li>Ayuda</li>
+              <li>Politica de Privacidad</li>
+              <li>Mapa del sitio</li>
+           </ul>
+        </span>
+        <span>
+            <a class="contacto" href="Nosotros">Acerca de nosotros</a>
+           <ul>
+              <li>Acerca de GUILD</li>
+              <li>SIC</li>
+              <li>PQRS</li>
+           </ul>
+        </span>
+        <span>
+           <a class="contacto" href="Contactanos">Contactanos</a>
+         <img src="https://s3-symbol-logo.tradingview.com/facebook--600.png" alt="Logito de facebook" width="70" height="70">
+         <img src="https://www.enter.co/wp-content/uploads/2016/11/Instagram_AppIcon1024.jpg" alt="Logito de instagram" width="70" height="70">
+         <img src="https://play-lh.googleusercontent.com/J8k5q78xv4R8Smi4vOE6iUphLvOz0efC-0lzoyGfd0KRUlAv4ekuCtlss6KBN-tMvEw" alt="Logito de twitter" width="70" height="70">
+        </span>
+    </footer>
+    <div class="layman-terms">
+       <span>Terminos y condiciones</span>
+       <span>Ayuda</span>
+       <span>Derechos Reservados 2021</span>
+    </div>
+     
+</body>
+</html>
