0c58d61a5390fe6d6a5e0f10834ecd3467511db0
yefer55 <91355978+yefer55@users.noreply.github.com>
2021-11-05 15:30:10 -0500

    Update ofertas_view.php

--- php/Views/Path_page/ofertas_view.php
+++ php/Views/Path_page/ofertas_view.php
@@ -1,70 +1,56 @@
 <html>
     <head>
         <title>OFERTAS</title>
-        <style>
-body
-{
-    margin-left: 10px;
-  margin-right: 10px;
-  margin-top: 30px;
-  margin-bottom: 10px;
-}
-
- table {
- font-size: 15;
- font-family: Segoe UI;
-}
-        </style>
+        <link rel="stylesheet" href="./Style-view.css">
     </head>
-    <body background="imagenes/fondo steam.jpeg">
-
-
-        <table Border="1" width="80%" background="imagenes/fondo-textura-papel-arrugado-negro-oscuro-cerca_99266-411.jpg" Align="center"  Class="tabla">
+    <body>
+        <form action="index.html" >
+        <table class="table">
             <tr>
-                <td Align="left">
-                <figure Align="center"><p> <img style="border:6px solid white" src="Imagenes/mini-ragnaros.jpg" Align="center" width="140" height="140" class="ImageBorder"> </p></figure>
-                    <p style="color: white;" Align="center"> Mini Ragnaros</p>
-                    <p style="color: white;" Align="center"> Antes: $38.744</p>
-                    <p style="color: white;" Align="center"> Ahora: $34.869</p>
-                    <p><p><center><input type="button" name="boton" value="VER MÁS" style="width:120px; height:30px; background-color: white;"></center></p></p>
+                <td >
+                <figure ><p> <img src="Imagenes/mini-ragnaros.jpg" class="ImageBorder"> </p>
+                    <p class="Text" > Mini Ragnaros</p>
+                    <p  class="Text"> Antes: $38.744</p>
+                    <p  class="Text"> Ahora: $34.869</p>
+                    <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
                 <td>
-                    <figure Align="center"><p> <img style="border:6px solid white" src="Imagenes/mini-kt.jfif" Align="center" width="140" height="140"> </p></figure>
-                 <p style="color: white;" Align="center"> Mini K.T</p>
-                    <p style="color: white;" Align="center"> Antes: $38.744</p>
-                    <p style="color: white;" Align="center">  Ahora: $34.869</p>
-                    <p><p><center><input type="button" name="boton" value="VER MÁS" style="width:120px; height:30px; background-color: white;"></center></p></p>
+                    <p > <img  src="Imagenes/mini-kt.jfif" class="ImageBorder" > </p>
+                 <p class="Text"> Mini K.T</p>
+                    <p class="Text"> Antes: $38.744</p>
+                    <p class="Text">  Ahora: $34.869</p>
+                 <p class="button"><button type="submit" >VER MAS </button></p>
                 </td>
                 <td>
-                    <figure Align="center"><p> <img style="border:6px solid white" src="Imagenes/karambit.jpg" Align="center" width="140" height="140"> </p></figure>
-                    <p style="color: white;" Align="center"> Karambit</p>
-                    <p style="color: white;" Align="center"> Antes: $2'324.640</p>
-                    <p style="color: white;" Align="center">  Ahora: $2'092.176</p>
-                    <p><p><center><input type="button" name="boton" value="VER MÁS" style="width:120px; height:30px; background-color: white;"></center></p></p>
+                <p> <img  src="Imagenes/karambit.jpg" class="ImageBorder" > </p>
+                <p class="Text"> Karambit</p>
+                <p class="Text"> Antes: $2'324.640</p>
+                <p class="Text">  Ahora: $2'092.176</p>
+                <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
             </tr>
 
-            <tr>
+            <tr >
                 <td>
-                    <figure Align="center"><p> <img style="border:6px solid white" src="Imagenes/glock-18.jfif" Align="center" width="140" height="140"> </p></figure>
-                    <p style="color: white;" Align="center"> Glocok-18</p>
-                    <p style="color: white;" Align="center"> Antes: $220.840</p>
-                    <p style="color: white;" Align="center"> Ahora: $198.756</p>
-                    <p><p><center><input type="button" name="boton" value="VER MÁS" style="width:120px; height:30px; background-color: white;"></center></p></p>
+                <p> <img  src="Imagenes/glock-18.jfif" class="ImageBorder"></p>
+                    <p  class="Text"> Glocok-18</p>
+                    <p  class="Text"> Antes: $220.840</p>
+                    <p  class="Text"> Ahora: $198.756</p>
+                    <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
                 <td>
-                    <figure Align="center"><p> <img style="border:6px solid white" src="Imagenes/tormento-guerra.jfif" Align="center" width="140" height="140"> </p></figure>
-                    <p style="color: white;" Align="center"> Tormento De Guerrra</p>
-                    <p style="color: white;" Align="center"> Antes: $116.232</p>
-                    <p style="color: white;" Align="center">  Ahora: $104.608</p>
-                    <p><p><center><input type="button" name="boton" value="VER MÁS" style="width:120px; height:30px; background-color: white;"></center></p></p>
+                   <p> <img  src="Imagenes/tormento-guerra.jfif" class="ImageBorder"> </p>
+                    <p class="Text"> Tormento De Guerrra</p>
+                    <p class="Text"> Antes: $116.232</p>
+                    <p class="Text" >  Ahora: $104.608</p>
+                    <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
                 <td>
-                    <figure Align="center"><p> <img src="Imagenes/Incinerador.jpg" Align="center" width="140" height="140"> </p></figure>
-                    <p style="color: white;" Align="center"> Incinerador Metalescama</p>
-                    <p style="color: white;" Align="center"> Antes: $96.860</p>
-                    <p style="color: white;" Align="center">  Ahora: $87.174</p>
-                    <p><p><center><input type="button" name="boton" value="VER MÁS" style="width:120px; height:30px; background-color: white;"></center></p></p>
+                   <p> <img src="Imagenes/Incinerador.jpg" class="ImageBorder" > </p>
+                    <p class="Text" > Incinerador Metalescama</p>
+                    <p class="Text"> Antes: $96.860</p>
+                    <p  class="Text">  Ahora: $87.174</p>
+                    <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
             </tr>
 
