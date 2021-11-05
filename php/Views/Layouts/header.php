3078836cadff2ddedc85b0cadd373aaf0d69ddca
miguelangelmolinaforero <68454714+miguelangelmolinaforero@users.noreply.github.com>
2021-11-04 19:30:16 -0500

    Invocación de estilos de la pestaña de 'Categoria'

--- php/Views/Layouts/header.php
+++ php/Views/Layouts/header.php
@@ -1,6 +1,6 @@
-    <!-- PHP -->
+<!-- PHP -->
 
-    <?php 
+<?php 
         // require_once('db_connection.php');
         // session_start();
         // $id_user = $_SESSION['user_id'];
@@ -10,45 +10,44 @@
         // $query->execute();
         // $result = $query->fetch(PDO::FETCH_ASSOC);
     ?>
-    
-    <!-- HEADER -->
-    <header class="header">
-        <figure class="logo">
-            <h1 class="page_name">GUILD</h1>
+
+<!-- HEADER -->
+<figure class="logo">
+    <h1 class="page_name">
+        <a style="text-decoration: none; color: white; " href="?controller=path&action=index">GUILD</a>
+    </h1>
+</figure>
+<nav class="page_navigation">
+    <input type="text" name="search_bar" id="search_bar" class="search_bar" placeholder="Busca productos, juegos, más" size="60">
+    <ul class="page_options">
+        <li><a href="?controller=path&action=categoria">Categorias</a></li>
+        <li><a href="">Mis Compras</a></li>
+        <li><a href="">Ofertas</a></li>
+        <li><a href="">Vender</a></li>
+    </ul>
+</nav>
+<div class="user_data">
+    <div class="session_options">
+        <figure class="user_logo">
+            <span class="icon-user"></span>
         </figure>
-        <nav class="page_navigation">
-            <input type="text" name="search_bar" id="search_bar" class="search_bar" placeholder="Busca productos, juegos, más" size="60">
-            <ul class="page_options">
-                <li><a href="">Categorias</a></li>
-                <li><a href="">Favoritos</a></li>
-                <li><a href="">Mis Compras</a></li>
-                <li><a href="">Ofertas</a></li>
-                <li><a href="">Vender</a></li>
-            </ul>
-        </nav>
-        <div class="user_data">
-            <div class="session_options">
-                <figure class="user_logo">
-                    <span class="icon-user"></span>
-                </figure>
-                <h3 class="name_user">
-                    <a href="../../../../GUILD/php/login.php">
-                    inicia sesion
+        <h3 class="name_user">
+            <a style="text-decoration: none; color: white;" href="../../../../GUILD/php/login.php">
+                    Inicia sesion
                 </a>
-            </h3>
-            </div>
-            <div class="country_user_section">
-                <select class="country_user" name="country_user" id="country_user">
-                    <option value="Colombia">Colombia</option>
-                    <option value="Argentina">Argentina</option>
-                    <option value="Brasil">Brasil</option>
-                    <option value="Bolivia">Bolivia</option>
-                    <option value="Mexico">Mexico</option>
-                    <option value="Venezuela">Venezuela</option>
-                    <option value="Panamá">Panamá</option>
-                    <option value="Puerto Rico">Puerto Rico</option>
-                    <option value="Chile">Chile</option>
-                </select>
-            </div>
-        </div>
-    </header>
+        </h3>
+    </div>
+    <div class="country_user_section">
+        <select class="country_user" name="country_user" id="country_user">
+            <option value="Colombia">Colombia</option>
+            <option value="Argentina">Argentina</option>
+            <option value="Brasil">Brasil</option>
+            <option value="Bolivia">Bolivia</option>
+            <option value="Mexico">Mexico</option>
+            <option value="Venezuela">Venezuela</option>
+            <option value="Panamá">Panamá</option>
+            <option value="Puerto Rico">Puerto Rico</option>
+            <option value="Chile">Chile</option>
+        </select>
+    </div>
+</div>
--- php/Views/Layouts/layout.php
+++ php/Views/Layouts/layout.php
@@ -7,10 +7,14 @@
 
     <!-- LINK DEL LOGO -->
     <link rel="shortcut icon" href="../images/logo_pagina_pestania.jpg" type="image/x-icon">
-    <!-- INCLUIR CSS -->
+
+    <!-- INCLUIR CSS PRINCIPAL -->
     <link rel="stylesheet" href="../../../../GUILD/assets/css/styles_main_page.css">
 
-    <!-- INLCUIR BOOTSTRAP -->
+	<!-- INCLUIR CSS CATEGORIAS -->
+	<link rel="stylesheet" href="../../../../GUILD/assets/css/categorias.css">
+    
+	<!-- INLCUIR BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
@@ -19,7 +23,7 @@
 </head>
 <body>
 	<!-- header -->
-	<header>
+	<header class="header">
 		<?php require_once('header.php'); ?>
 	</header>
 
