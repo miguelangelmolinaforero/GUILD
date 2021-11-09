86eb19125a6fcea2582dba3787a32566f19ce2ab
miguelangelmolinaforero <miguelangelmolinaforero@gmail.com>
2021-11-09 02:55:37 -0500

    Modificacion estilso de pestaña ofertas

--- php/Views/Path_page/ofertas_view.php
+++ php/Views/Path_page/ofertas_view.php
@@ -1,60 +1,57 @@
-<html>
-    <head>
-        <title>OFERTAS</title>
-        <link rel="stylesheet" href="../../../../GUILD/assets/css/Style-views.css">
-    </head>
-    <body>
-        <form action="index.html" >
+<link rel="stylesheet" href="../../../../GUILD/assets/css/Style-views.css">
+<div class="ofertas_container">
+
+
+    <form action="index.html">
         <table class="table">
             <tr>
-                <td >
-                <figure ><p> <img src="../../../../GUILD/images/mini-ragnaros.jpg" class="ImageBorder"> </p>
-                    <p class="Text" > Mini Ragnaros</p>
-                    <p  class="Text"> Antes: $38.744</p>
-                    <p  class="Text"> Ahora: $34.869</p>
+                <td>
+                    <figure>
+                        <p> <img src="../../../../GUILD/images/mini-ragnaros.jpg" class="ImageBorder"> </p>
+                        <p class="Text"> Mini Ragnaros</p>
+                        <p class="Text"> Antes: $38.744</p>
+                        <p class="Text"> Ahora: $34.869</p>
+                        <p class="button"><button type="submit">VER MAS </button></p>
+                </td>
+                <td>
+                    <p> <img src="../../../../GUILD/images/mini-kt.jfif" class="ImageBorder"> </p>
+                    <p class="Text"> Mini K.T</p>
+                    <p class="Text"> Antes: $38.744</p>
+                    <p class="Text"> Ahora: $34.869</p>
                     <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
                 <td>
-                    <p > <img  src="../../../../GUILD/images/mini-kt.jfif" class="ImageBorder" > </p>
-                 <p class="Text"> Mini K.T</p>
-                    <p class="Text"> Antes: $38.744</p>
-                    <p class="Text">  Ahora: $34.869</p>
-                 <p class="button"><button type="submit" >VER MAS </button></p>
-                </td>
-                <td>
-                <p> <img  src="../../../../GUILD/images/karambit.jpg" class="ImageBorder" > </p>
-                <p class="Text"> Karambit</p>
-                <p class="Text"> Antes: $2'324.640</p>
-                <p class="Text">  Ahora: $2'092.176</p>
-                <p class="button"><button type="submit">VER MAS </button></p>
+                    <p> <img src="../../../../GUILD/images/karambit.jpg" class="ImageBorder"> </p>
+                    <p class="Text"> Karambit</p>
+                    <p class="Text"> Antes: $2'324.640</p>
+                    <p class="Text"> Ahora: $2'092.176</p>
+                    <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
             </tr>
 
-            <tr >
+            <tr>
                 <td>
-                <p> <img  src="../../../../GUILD/images/glock-18.jfif" class="ImageBorder"></p>
-                    <p  class="Text"> Glocok-18</p>
-                    <p  class="Text"> Antes: $220.840</p>
-                    <p  class="Text"> Ahora: $198.756</p>
+                    <p> <img src="../../../../GUILD/images/glock-18.jfif" class="ImageBorder"></p>
+                    <p class="Text"> Glocok-18</p>
+                    <p class="Text"> Antes: $220.840</p>
+                    <p class="Text"> Ahora: $198.756</p>
                     <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
                 <td>
-                   <p> <img  src="../../../../GUILD/images/tormento-guerra.jfif" class="ImageBorder"> </p>
+                    <p> <img src="../../../../GUILD/images/tormento-guerra.jfif" class="ImageBorder"> </p>
                     <p class="Text"> Tormento De Guerrra</p>
                     <p class="Text"> Antes: $116.232</p>
-                    <p class="Text" >  Ahora: $104.608</p>
+                    <p class="Text"> Ahora: $104.608</p>
                     <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
                 <td>
-                   <p> <img src="../../../../GUILD/images/Incinerador.jpg" class="ImageBorder" > </p>
-                    <p class="Text" > Incinerador Metalescama</p>
+                    <p> <img src="../../../../GUILD/images/Incinerador.jpg" class="ImageBorder"> </p>
+                    <p class="Text"> Incinerador Metalescama</p>
                     <p class="Text"> Antes: $96.860</p>
-                    <p  class="Text">  Ahora: $87.174</p>
+                    <p class="Text"> Ahora: $87.174</p>
                     <p class="button"><button type="submit">VER MAS </button></p>
                 </td>
             </tr>
 
-            </table>
-            </body>
-            </html>
-            
+        </table>
+</div>
